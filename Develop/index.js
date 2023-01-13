7b7c4e295e31d5b25b2acf5df6a71c757c0c36fd
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-01-12 18:18:01 -0600

    installed node, started writing init, write to file, and generate markdown functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquire = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "what is your github username?",
+    name: "title",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const markdown = generateMarkdown(data);
+  return fs.writeFileSync(fileName, markdown);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquire.prompt(questions).then((response) => {
+    console.log(response);
+    writeToFile("README.md", response);
+  });
+}
 
 // Function call to initialize app
 init();
