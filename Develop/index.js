c7709a4df915815ca4b79ef70e391761a5127789
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-01-14 17:27:47 -0600

    finished adding prompts, completed the license functions, and started working on markdown formatting

--- Develop/index.js
+++ Develop/index.js
@@ -7,8 +7,49 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
   {
     type: "input",
-    message: "what is your github username?",
+    message: "what is the title of your project?",
     name: "title",
+  },
+  {
+    type: "list",
+    message: "what license do you want for your readme?",
+    name: "license",
+    choices: ["none", "MIT", "APM", "Bower"],
+  },
+  {
+    type: "input",
+    message: "enter the description of your project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "enter installation instructions",
+    name: "intallation",
+  },
+  {
+    type: "input",
+    message: "enter usage information",
+    name: "usageInfo",
+  },
+  {
+    type: "input",
+    message: "enter your contribution guidelines:",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "enter the tests",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "what is your github username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "what is your email?",
+    name: "email",
   },
 ];
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,22 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return `this will be a badge`;
+  if (license !== "none") {
+    return `<img src="https://img.shields.io/badge/license-${license}-blue.svg">`;
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `https://opensource.org/licenses/${license})`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License: 
+
+    This project is licensed under ${license}
+
+    ${renderLicenseBadge(license)}
+
+    ${renderLicenseLink(license)}`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} this is the title
-  ${renderLicenseBadge(data.license)}
-`;
+  return `# ${data.title}
+
+  ## Table of Contents
+  1.[License](#license)
+
+  2.[Description](#description)
+
+  3.[Installation](#installation)
+
+  4.[Usage Information](#usage-information)
+
+  5.[Contribution Guidelines](#contributing)
+
+  6.[Tests](#tests)
+
+  7.[Questions](#questions)
+
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Description:
+  ${data.description}
+
+  ## Installation:
+  ${data.installation}
+
+  ## Usage Information:
+  ${data.usageInfo}
+
+  ## Contribution Guidelines:
+  ${data.contributing}
+
+  ## Tests:
+  ${data.tests}
+
+  ## Questions:
+  GitHub Profile: https://github.com/${data.username} 
+  If you have any additional questions, please email me at ${data.email}.
+  `;
 }
 
 module.exports = generateMarkdown;
